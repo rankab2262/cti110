df82446ba9cf88cab195f679883616c6a664d04f
rankab2262 <36566409+rankab2262@users.noreply.github.com>
2018-02-17 08:44:09 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_RankaBradley.py
@@ -0,0 +1,11 @@
+
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Name
+# Date
+
+totalSales= int(input("What are the projected amount of sales?: "))
+annualProfit = (.23)
+total=totalSales*annualProfit
+
+print("the profit is",total)
